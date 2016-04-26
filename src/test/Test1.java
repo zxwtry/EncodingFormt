11b91e7c053c575aa9b750779de3440023a7e8ae
zxwtry <zxwtry@qq.com>
2016-04-26 20:28:23 +0800

    增加一些测试

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -27,11 +27,8 @@ public class Test1 {
 		parse2UTF_8(file01, file01_Create);
 		System.out.println(file01.exists());
 		System.out.println(Charset.defaultCharset());
-		
-		
-		
-		
 	}
+	
 	public static void parse2UTF_8(File file, File destFile) throws IOException {
 		StringBuffer msg = new StringBuffer();
 		// 读写对象
@@ -58,13 +55,75 @@ public class Test1 {
 			byte[] string_utf8_bytes = string_utf8.getBytes("utf-8");
 			String string_gbk = new String(string_utf8_bytes, "gbk");
 			System.out.println(string_gbk);
-			createNewFileAndWriteString("C:/data/newTest01.txt", "utf-8", string_gbk);
+			createNewFileAndWriteString("C:/data/newTest01.txt", "gbk", string_gbk);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private void createNewFileAndWriteString(String fileURI, String charset, String toWrite) {
+	@Test
+	public void getFromTest01 () {
+//		readFromFile("C:/data/B.java", "GBK");
+//		String string = "给定一个字符串，要求把字符串前面的若干个字符移动到字符串的尾部，"+
+//						"如把字符串“abcdef”前面的2个字符'a'和'b'移动到字符串的尾部，"+
+//						"使得原字符串变成字符串“cdefab”。请写一个函数完成此功能，"+
+//						"要求对长度为n的字符串操作的时间复杂度为 O(n)，空间复杂度为 O(1)。";
+//		System.out.println(convertStringToTargetCharset(string, "UTF-8", "GBK"));
+		String middleString = readStringFromFileByDefaultCharset("C:/data/B.java");
+		System.out.println(convertStringToTargetCharset(middleString, "GBK", "UTF-8"));
+	}
+	
+	String readStringFromFileByDefaultCharset(String fileURI) {
+		StringBuffer stringBuffer = new StringBuffer();
+		int count = 0;
+		byte[] buff = new byte[16];
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream(new File(fileURI));
+			while ( (count = fileInputStream.read(buff)) != -1 ) {
+				stringBuffer.append(new String(buff, 0, count));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (null != fileInputStream) {
+				try {
+					fileInputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return stringBuffer.toString();
+	}
+	
+	String convertStringToTargetCharset(String string, String thisCharset, String targetCharset) {
+		String returnString = null;
+		try {
+			returnString = new String(string.getBytes(thisCharset),targetCharset);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return returnString;
+	}
+	
+	String readFromFile(String fileURI, String charset) {
+		StringBuffer stringBuffer = new StringBuffer();
+		File file = new File(fileURI);
+		try {
+			FileInputStream fileInputStream = new FileInputStream(file);
+			byte[] buff = new byte[4096];
+			int count = fileInputStream.read(buff);
+			String string_read = new String(buff, 0, count, charset);
+			System.out.println(string_read);
+			fileInputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return stringBuffer.toString();
+	}
+	
+	void createNewFileAndWriteString(String fileURI, String charset, String toWrite) {
 		File file = new File(fileURI);
 		if (file.exists()) {
 			file.delete();
